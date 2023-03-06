c8a3204fca5743df9385cb6a45be84067b37470b
Gustavo Silvano <gustavolsilvano@gmail.com>
2023-03-06 11:04:19 -0300

    feat: adding products_overview_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_shop/screens/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,25 +15,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      routes: {
+        '/': (_) => ProductOverviewScreen(),
+        ProductOverviewScreen.routeName: (_) => ProductOverviewScreen()
+      },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('MyShop'),
-        ),
-        body: const Center(child: Text('Let\s build a shopping app!')));
   }
 }
